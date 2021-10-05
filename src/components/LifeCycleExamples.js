ad7fb7b554056ab424ca954e3a5e196990691cb6
Diego Castro <diego.castro@evolvingweb.ca>
2021-10-05 11:14:48 -0400

    Bar Chart: Life Cycle chart. - Divisions chart.

--- src/components/LifeCycleExamples.js
+++ src/components/LifeCycleExamples.js
@@ -1,9 +1,17 @@
 import LifeCycle from './charts/LifeCycle';
-import { lifeCycleMockData, lifeCycleMultipleMockData } from '../mock-data/life-cycle.mock';
+import {
+  divisionsMockData,
+  lifeCycleMockData,
+  lifeCycleMultipleMockData,
+} from '../mock-data/life-cycle.mock';
 
 const LifeCycleExamples = () => {
   return (
       <>
+        <section>
+          <h2>Divisions</h2>
+          <LifeCycle data={divisionsMockData} height={360} />
+        </section>
         <section>
           <h2>Life Cycle</h2>
           <LifeCycle data={lifeCycleMockData} />
--- src/mock-data/life-cycle.mock.js
+++ src/mock-data/life-cycle.mock.js
@@ -41,3 +41,37 @@ export const lifeCycleMultipleMockData = {
     },
   ],
 };
+
+export const divisionsMockData = {
+  labels: ['Design 1', 'Design 2', 'Design 3'],
+  datasets: [
+    {
+      label: 'Concrete',
+      data: [-2900, 1100, -11000],
+    },
+    {
+      label: 'Masonry',
+      data: [2000, 3000, 4500],
+    },
+    {
+      label: 'Metals',
+      data: [15000, 12000, 20000],
+    },
+    {
+      label: 'Wood/Plastics/Composites',
+      data: [40000, 30000, 45000],
+    },
+    {
+      label: 'Thermal and Moisture Protection',
+      data: [15000, 10000, 22000],
+    },
+    {
+      label: 'Openings and Glazing',
+      data: [5000, 6000, 7800],
+    },
+    {
+      label: 'Finishes',
+      data: [14000, 12000, 15300],
+    },
+  ],
+};
