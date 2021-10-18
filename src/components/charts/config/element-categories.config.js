f6074e1a5bd57112d1eb9a68720800a8e2d12618
Diego Castro <diego.castro@evolvingweb.ca>
2021-10-18 08:33:17 -0400

    Move components from js to jsx files.

--- src/components/charts/config/element-categories.config.js
+++ src/components/charts/config/element-categories.config.js
@@ -1,15 +1,5 @@
 import { extendConfig } from './config';
 
-export const COLORS = [
-  '#A3BA19',
-  '#377B65',
-  '#C1E8C3',
-  '#2346A0',
-  '#67BAB3',
-  '#F05D5E',
-  '#FC8F40',
-];
-
 export const OPTIONS = extendConfig({
   scales: {
     y:{
