b011bd1c9c9e80e939d01295af88e048b57577a6
Diego Castro <diego.castro@evolvingweb.ca>
2021-10-20 18:54:27 -0400

    Design view - Updating left side filters.

--- src/components/views/Design.jsx
+++ src/components/views/Design.jsx
@@ -53,20 +53,15 @@ const DesignView = ({ id, design, onEdit, onShowCompare }) => {
 
   const containerClasses = classNames(
       'bg-white',
-      'px-6',
-      'py-6',
-      'lg:px-24',
-      'lg:py-14',
       'min-h-screen',
       'flex',
       'flex-col',
   );
   const componentClasses = classNames(
-      'max-w-screen-xl',
-      'mx-auto',
       'grid',
-      'grid-cols-1 lg:grid-cols-4',
+      'grid-cols-4',
       'gap-14',
+      'place-items-start',
   );
 
   const titleClasses = classNames(
@@ -91,9 +86,8 @@ const DesignView = ({ id, design, onEdit, onShowCompare }) => {
   return (
       <section className={containerClasses}>
         <section className={componentClasses}>
-          <div>
-            <h3 className="text-black-olive font-bold text-lg">Multi-Family Design Options</h3>
-            <div className="mt-7">
+          <div className="pl-6 pr-6 pt-6 bg-pale-gray xl:pl-14 xl:pr-12 xl:pt-14 min-h-screen">
+            <div className="mb-8">
               <Button
                   icon={PencilIcon}
                   onClick={onEditClick}
@@ -102,6 +96,7 @@ const DesignView = ({ id, design, onEdit, onShowCompare }) => {
                 Edit
               </Button>
             </div>
+            <h3 className="text-black-olive font-bold text-lg">Multi-Family Design Options</h3>
             <div className="mt-7">
               {
                 _map(FILTER_CATEGORIES, (columnData) => {
@@ -123,7 +118,7 @@ const DesignView = ({ id, design, onEdit, onShowCompare }) => {
 
                               return (
                                   <p key={filter} className="mt-0">
-                                    <span className="inline-block px-2 py-1 bg-pale-gray rounded-xl text-xxs">
+                                    <span className="inline-block px-2 py-1 bg-white rounded-xl text-xxs">
                                       {filterValue}
                                     </span>
                                   </p>
@@ -137,32 +132,35 @@ const DesignView = ({ id, design, onEdit, onShowCompare }) => {
               }
             </div>
           </div>
-          <div className="col-span-2 flex flex-col">
-            <img src={imageUrl} alt={imageAlt} className="mx-auto" width={324} height={506} />
-            <div className="mt-auto">
-              <h3 className="uppercase font-semibold mt-7 mb-4 flex align-center">
-                Building Embodied Carbon Rating
-              </h3>
-              <CarbonRating value={embodiedCarbonRating} />
+          <div className="grid grid-cols-3 gap-14 col-span-3 pt-14 pr-24">
+            <div className="col-span-2 flex flex-col">
+              <img src={imageUrl} alt={imageAlt} className="mx-auto" width={324} height={506} />
+              <div className="mt-auto">
+                <h3 className="uppercase font-semibold mt-7 mb-4 flex align-center">
+                  Building Embodied Carbon Rating
+                </h3>
+                <CarbonRating value={embodiedCarbonRating} />
+              </div>
             </div>
-          </div>
-          <div className="flex align-start flex-col">
-            <h2 className={titleClasses}>Design {id}</h2>
-            <div>
-              <Button icon={DiagonalArrows} onClick={onShowCompareClick}>Save & view all metrics</Button>
-            </div>
-            <div className="mt-auto mb-3">
-              <h3 className="uppercase font-semibold mt-7 mb-4 flex align-center">
-                Sum of Embodied Carbon
-              </h3>
-              <p className="text-2xl font-normal">
-                {sumEmbodiedCarbonParts[0]}
-                <span>{sumEmbodiedCarbonParts.length > 1 ? '.' : null}</span>
-                <span className="text-xs">{sumEmbodiedCarbonParts.length > 1 ? sumEmbodiedCarbonParts[1] : null}</span>
-                <span className="ml-1 text-xs font-bold">KgCO2eq/m2</span>
-              </p>
-              <div className="relative -left-2">
-                <Summary data={chartData} height={80} options={chartOptions} />
+            <div className="flex align-start flex-col">
+              <h2 className={titleClasses}>Design {id}</h2>
+              <div>
+                <Button icon={DiagonalArrows} onClick={onShowCompareClick}>Save & view all metrics</Button>
+              </div>
+              <div className="mt-auto mb-3">
+                <h3 className="uppercase font-semibold mt-7 mb-4 flex align-center">
+                  Sum of Embodied Carbon
+                </h3>
+                <p className="text-2xl font-normal">
+                  {sumEmbodiedCarbonParts[0]}
+                  <span>{sumEmbodiedCarbonParts.length > 1 ? '.' : null}</span>
+                  <span
+                      className="text-xs">{sumEmbodiedCarbonParts.length > 1 ? sumEmbodiedCarbonParts[1] : null}</span>
+                  <span className="ml-1 text-xs font-bold">KgCO2eq/m2</span>
+                </p>
+                <div className="relative -left-2">
+                  <Summary data={chartData} height={80} options={chartOptions} />
+                </div>
               </div>
             </div>
           </div>
